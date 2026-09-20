ae7dd7451fd22a0feee57562a87fe68645c4653a
DAFTCCC <daftccc@gmail.com>
2026-09-19 21:13:52 -1000

    Bump web version for invite diagnostics

--- version.js
+++ version.js
@@ -1,6 +1,6 @@
 window.TCCC_BUILD = Object.freeze({
-  versionName: '3.1.1-web.1',
-  versionCode: 30101,
+  versionName: '3.1.2-web.1',
+  versionCode: 30102,
   target: 'web-pwa'
 });
 const APP_VERSION = window.TCCC_BUILD.versionName;
