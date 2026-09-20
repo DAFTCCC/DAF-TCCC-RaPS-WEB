3179862c16b1df5066978b38204cca8408280c4c
DAFTCCC <daftccc@gmail.com>
2026-09-19 21:35:19 -1000

    Bump web version for membership start fix

--- version.js
+++ version.js
@@ -1,6 +1,6 @@
 window.TCCC_BUILD = Object.freeze({
-  versionName: '3.1.2-web.1',
-  versionCode: 30102,
+  versionName: '3.1.3-web.1',
+  versionCode: 30103,
   target: 'web-pwa'
 });
 const APP_VERSION = window.TCCC_BUILD.versionName;
