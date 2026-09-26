6f65ad87528fda4bd43614fadba7871aabd591e8
DAFTCCC <daftccc@gmail.com>
2026-09-25 22:02:31 -1000

    Bump web version to 3.4.9

--- version.js
+++ version.js
@@ -1,6 +1,6 @@
 window.TCCC_BUILD = Object.freeze({
-  versionName: '3.4.8-web.1',
-  versionCode: 30408,
+  versionName: '3.4.9-web.1',
+  versionCode: 30409,
   target: 'web-pwa'
 });
 const APP_VERSION = window.TCCC_BUILD.versionName;
