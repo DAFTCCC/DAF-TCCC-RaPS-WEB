d26fbf311602055c67b53c17e600d0ec7feb1e24
DAFTCCC <daftccc@gmail.com>
2026-09-25 20:36:47 -1000

    Phase 7D: bump RaPS web version to 3.4.7

--- version.js
+++ version.js
@@ -1,6 +1,6 @@
 window.TCCC_BUILD = Object.freeze({
-  versionName: '3.4.6-web.1',
-  versionCode: 30406,
+  versionName: '3.4.7-web.1',
+  versionCode: 30407,
   target: 'web-pwa'
 });
 const APP_VERSION = window.TCCC_BUILD.versionName;
